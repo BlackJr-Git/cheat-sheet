7e357b039894103b66894d7b6c26c8fe6a985e41
Junior Asosa <juniorassosa@gmail.com>
2024-09-28 13:45:08 +0100

    refactor(category/[categoryId]) : make it a server components by changing the use of useParmas hook

--- src/app/categories/[categoryId]/page.tsx
+++ src/app/categories/[categoryId]/page.tsx
@@ -1,29 +1,20 @@
-"use client";
-import { useParams } from "next/navigation";
-import { useState, useEffect } from "react";
 import axios from "axios";
-import { CategoryType } from "@/types";
-
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-function Page() {
-  const { categoryId } = useParams();
-  const [category, setCategory] = useState({} as CategoryType);
+async function Page({ params }: { params: { categoryId: string } }) {
+  const { categoryId } = params;
 
-  useEffect(() => {
-    async function getCategory() {
-      const { data } = await axios.get(
-        `${apiUrl}/api/category/${categoryId}`
-      );
-      setCategory(data);
-      console.log(data);
-      
+  async function getCatagories() {
+    try {
+      const { data } = await axios.get(`${apiUrl}/api/category/${categoryId}`);
+      return data.categories;
+    } catch (error) {
+      return [];
     }
+  }
 
-    getCategory();
-  }, [categoryId]);
+  const category = (await getCatagories()) || {};
 
-  // return <>Category</>
   return <div>{category.name}</div>;
 }
 
