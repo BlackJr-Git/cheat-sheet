c75bf99fe27c3478bcc48235b64356a8a14bc7b2
Junior Asosa <junior.ngoy@quantumvertex.cd>
2024-10-10 17:08:20 +0100

    refactor(homePage) : switch to new next api metada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,51 +8,40 @@ import {
 } from "@/components";
 import Head from "next/head";
 
+export const metadata = {
+  title:
+    "The Cheat Sheet | Toutes les ressources qui vous rendent plus productif",
+  description:
+    "Trouver toutes les ressources dont vous avez besoin pour gagner du temps et de la productivité",
+  openGraph: {
+    type: "website",
+    title: "The Cheat Sheet",
+    description:
+      "Trouver toutes les ressources dont vous avez besoin pour gagner du temps et de la productivité",
+    url: "https://the-cheat-sheet.vercel.app/",
+    siteName: "The Cheat Sheet",
+    images: [
+      {
+        url: "https://the-cheat-sheet.vercel.app/homepage-1.png",
+        width: 1200,
+        height: 630,
+        alt: "The Cheat Sheet Homepage",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "The Cheat Sheet",
+    description:
+      "Trouver toutes les ressources dont vous avez besoin pour gagner du temps et de la productivité",
+    images: ["https://the-cheat-sheet.vercel.app/homepage-1.png"],
+    site: "@JuniorAsosa",
+  },
+};
+
 export default function Home() {
   return (
     <div>
-      <Head>
-        <Head>
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="The Cheat Sheet" />
-          <meta
-            property="og:description"
-            content="Trouver tous les sites ou outils dont vous avez besoin pour gagner du temps et de la productivité"
-          />
-          <meta
-            property="og:image"
-            content="https://the-cheat-sheet.vercel.app/homepage-1.png"
-          />
-          <meta property="og:image:width" content="1200" />
-          <meta property="og:image:height" content="630" />
-          <meta
-            property="og:url"
-            content="https://the-cheat-sheet.vercel.app/"
-          />
-          <meta property="og:site_name" content="The Cheat Sheet" />
-
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="The Cheat Sheet" />
-          <meta
-            name="twitter:description"
-            content="Trouver tous les sites ou outils dont vous avez besoin pour gagner du temps et de la productivité"
-          />
-          <meta
-            name="twitter:image"
-            content="https://the-cheat-sheet.vercel.app/homepage-1.png"
-          />
-          <meta name="twitter:site" content="@JuniorAsosa" />
-
-          <title>
-            The Cheat Sheet | Tous les sites et outils qui vous rendent plus
-            productif
-          </title>
-          <meta
-            name="description"
-            content="Trouver tous les sites ou outils dont vous avez besoin pour gagner du temps et de la productivité"
-          />
-        </Head>
-      </Head>
       <main className="py-12">
         <HeroSection />
         <Categories />
